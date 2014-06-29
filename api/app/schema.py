c4c5961b35e061d74043850039d1d349ffca3b4e
Sam <unclesaamm@gmail.com>
2014-06-29 03:21:30 -0500

    yelp integration + any search

--- api/app/schema.py
+++ api/app/schema.py
@@ -15,7 +15,6 @@ class Inspection(Base):
     City = Column(String)
     State = Column(String)
     Zip = Column(Integer)
-#    Inspection_Date = Column(Date)
     Inspection_Date = Column(String)
     Inspection_Type = Column(String)
     Results = Column(String)
@@ -23,3 +22,16 @@ class Inspection(Base):
     Latitude = Column(Float)
     Longitude = Column(Float)
 
+class Yelp(Base):
+    __tablename__ = 'yelp'
+    yelp_id = Column(String, primary_key=True)
+    db_name = Column(String)
+    db_addr = Column(String)
+    yelp_name = Column(String)
+    avg_rating = Column(Float)
+    review_count = Column(Integer)
+    photo_url = Column(String)
+    rating_img_url = Column(String)
+    yelp_address = Column(String)
+    zip_code = Column(String)
+    phone = Column(String)
