715454486e912df2707fb4789b88e4d7addf7065
Sam <unclesaamm@gmail.com>
2014-06-29 04:33:56 -0500

    implemented /instant path for autocomplete

--- api/app/views.py
+++ api/app/views.py
@@ -13,7 +13,6 @@ from sqlalchemy.sql import func
 from helpers import haversine, get_rating, get_geo, get_yelp_id
 import requests
 import urllib
-
 from settings import gkey, ykey
 
 #google API query
@@ -21,23 +20,60 @@ gquery="https://maps.googleapis.com/maps/api/place/textsearch/json?query={q}&key
 #yelp API query
 yquery = 'http://api.yelp.com/business_review_search?term={name}&location={addr}&limit=1&ywsid=' + ykey
 #google instant API
-ginstant = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input={substring}&types=establishmen&radius=500&key=' + gkey
+ginstant = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input={substring}&types=establishment&radius=50000&key=' + gkey
 
+#============================================================================
+# /instant
+# Google Places Autocomplete API endpoint
+# Arguments: query -- query
+#            long  -- longitude
+#            lat   -- latitude 
+#       
+#   if long and lat are not provided, the center of Chicago is used
+#   
+#   returns a JSON object that contains a list of matches, where each match is
+#   {
+#       'name': <name of institution>,
+#       'place_id': <Google Place ID of institution>
+#   }
+##===========================================================================
 @app.route('/instant')
 def instant():
     query = request.args.get('query', '', type=str)
-    ginstant.format({query:urllib.quote_plus(query)})
+    gq = ginstant.format(substring=urllib.quote_plus(query))
+    
+    longitude = request.args.get('long', '-87.625916', type=str)
+    latitude = request.args.get('lat', '41.903196', type=str)
 
-    longitude = request.args.get('long', '', type=str)
-    latitude = request.args.get('lat', '', type=str)
+    gq += '&location={lat},{lng}'.format(lat=latitude,
+            lng=longitude)
+    
+    try:
+        r = requests.get(gq)
+    except requests.ConnectionError:
+        print "ConnectionError in Google Instant API--check connection?"
+        return Response(json.dumps({}), mimetype='text/json')
 
-    if longitude and latitude:
-        query += 'loc={lat},{lng}'.format({
-                lat:latitude,
-                lng:longitude})
+    
+    if not r.ok:
+        print "Request to Google Instant API failed: {}.".format(r.code)
+        return Response(json.dumps({}), mimetype='text/json')
 
-
-    return Request(json.dumps(result, mimetype='text/json'))
+    
+    try:
+        j = r.json()
+    except JSONDecodeError:
+        print "JSONDecodeError in instant search:", r.text
+        return Response(json.dumps({}), mimetype='text/json')
+    
+    result = []
+    if j and j.has_key('predictions'):
+        for pred in j['predictions']:
+            item = {'name': pred['description'],
+                    'place_id': pred['id']}
+            result.append(item)
+        
+    return Response(json.dumps(result), mimetype='text/json')
 
 
 #============================================================================
@@ -91,9 +127,8 @@ def place():
             'yelp_rating_pic': ri[2],
             'yelp_review_count': ri[3],
             'name': ri[4],
-            'addr': ri[5].strip() +\
-                    ' ' +\
-                    ri[6].strip(),
+            'addr': ri[5].strip(),
+            'addr2': 'Chicago, IL, ' + ri[6].strip(), 
             'pic': ri[7]
             }
             
@@ -246,7 +281,7 @@ def check():
                 rating_pic = ''
                 review_count = 0
                 use_address = address
-                photo = 'http://placekitten.com/100/100'
+                photo = 'http://s3-media1.fl.yelpcdn.com/assets/2/www/img/5f69f303f17c/default_avatars/business_medium_square.png'
             results.append({
                     'name': use_name,
                     'address': use_address,
